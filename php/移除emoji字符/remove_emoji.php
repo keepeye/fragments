0d3301a95b18bc43975005ad354e61cc4af8dc6b
cxz <carlton.cheng@foxmail.com>
2015-11-09 14:33:23 +0800

    init

---
+++ php/移除emoji字符/remove_emoji.php
@@ -0,0 +1,9 @@
+/**
+ * 移除字符串中的emoji表情符号
+ * 事件：mysql整理字符集 utf8_unicode_ci 时，若字符串中有emoji，会报错。
+ */
+
+function remove_emoji($str)
+{
+	return preg_replace('/([0-9|#][\x{20E3}])|[\x{00ae}|\x{00a9}|\x{203C}|\x{2047}|\x{2048}|\x{2049}|\x{3030}|\x{303D}|\x{2139}|\x{2122}|\x{3297}|\x{3299}][\x{FE00}-\x{FEFF}]?|[\x{2190}-\x{21FF}][\x{FE00}-\x{FEFF}]?|[\x{2300}-\x{23FF}][\x{FE00}-\x{FEFF}]?|[\x{2460}-\x{24FF}][\x{FE00}-\x{FEFF}]?|[\x{25A0}-\x{25FF}][\x{FE00}-\x{FEFF}]?|[\x{2600}-\x{27BF}][\x{FE00}-\x{FEFF}]?|[\x{2900}-\x{297F}][\x{FE00}-\x{FEFF}]?|[\x{2B00}-\x{2BF0}][\x{FE00}-\x{FEFF}]?|[\x{1F000}-\x{1F6FF}][\x{FE00}-\x{FEFF}]?/u', '', $str);
+}
